0bb9508e2c4cae2144918bb4c4228c702c6dbda6
Yaroslav_Holovan <yaroslav.golovan@gmail.com>
2022-02-12 00:40:33 +0200

    update 2.2.8

--- app/src/main/java/com/univer/inform_catalog/MainActivity.java
+++ app/src/main/java/com/univer/inform_catalog/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static void main(String[] args) {
-        System.out.println("Sulima suka");
+        System.out.println("Hello");
     }
 
 }
